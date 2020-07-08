d4dd9b8c85c5c3ba255e7eb96fb47e68d74eea11
elikantor <eli.kantor@outlook.com>
2020-07-07 22:10:08 -0400

    protein-translation

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -3,6 +3,41 @@
 // convenience to get you started writing code faster.
 //
 
-export const translate = () => {
-  throw new Error("Remove this statement and implement this function");
+export const translate = (codon) => {
+  let proteins = []
+  if (!codon) return proteins
+  if (codon.length % 3 !== 0) throw new Error('Invalid codon')
+  let numOfCodons = codon.length / 3
+  let arr = []
+
+  for (let i = 0; i < numOfCodons; i++) {
+    arr.push(codon.slice(i*3,i*3+3))
+  }
+  
+  for (let i = 0; i < arr.length; i++) {
+    if(!HASH[arr[i]]) throw new Error('Invalid codon')
+    if(HASH[arr[i]] === 'STOP') break
+    proteins.push(HASH[arr[i]])
+  }
+  return proteins
 };
+
+const HASH = {
+  AUG: 'Methionine',
+  UUU: 'Phenylalanine',
+  UUC: 'Phenylalanine',
+  UUA: 'Leucine',
+  UUG: 'Leucine',
+  UCU: 'Serine',
+  UCC: 'Serine',
+  UCA: 'Serine',
+  UCG: 'Serine',
+  UAU: 'Tyrosine',
+  UAC: 'Tyrosine',
+  UGU: 'Cysteine',
+  UGC: 'Cysteine',
+  UGG: 'Tryptophan',
+  UAA: 'STOP',
+  UAG: 'STOP',
+  UGA: 'STOP'
+}
