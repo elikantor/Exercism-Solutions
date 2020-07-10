07fc099c0a32a997a03b4ed536a6cbe73adeeea1
elikantor <eli.kantor@outlook.com>
2020-07-10 09:43:21 -0400

    space-age

---
+++ javascript/space-age/space-age.js
@@ -0,0 +1,19 @@
+//
+// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const age = (planet, seconds) => {
+  return Math.round(seconds / 31557600 / PLANETS[planet] * 100) / 100
+};
+
+const PLANETS = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1.0,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132
+}
