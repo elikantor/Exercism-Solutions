f08f0ad3f93db66601e197c85748c3c60f20bbb3
elikantor <eli.kantor@outlook.com>
2020-07-13 19:08:38 -0400

    optimized panagram

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,8 +1,7 @@
 
 export const isPangram = (string) => {
-  let lowerCaseString = string.toLowerCase()
-  const includes = (letter) => lowerCaseString.includes(letter)
-  return ALPHABET.every(includes)
+  const lowerCaseString = string.toLowerCase();
+  return ALPHABET.every(letter => lowerCaseString.includes(letter));
 };
 
-const ALPHABET = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const ALPHABET = [..."abcdefghijklmnopqrstuvwxyz"]
