49e04993859d5e1c3f7b9d6c65ad8733a9329e31
elikantor <eli.kantor@outlook.com>
2020-09-08 09:48:02 -0400

    refactor using regex

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,35 +1,20 @@
 import { stringify } from "querystring";
 
 export const hey = (message) => {
-  let lastIndex = message.length-1
-  let empty = true
-  for(let i = 0; i < message.length - 1; i++) {
-    if(message.charCodeAt(i) >= 97 && message.charCodeAt(i) <= 122) {
-      while(message.charCodeAt(lastIndex) === 32) lastIndex--
-      if(message[lastIndex] === "?") {
-        return "Sure."
-      } else {
-        return "Whatever."
-      }
-    } else if (message[i+2] && message.charCodeAt(i+2) >= 65 && message.charCodeAt(i+2) <= 90) {
-      if(message[lastIndex] === "?") {
-        return "Calm down, I know what I\'m doing!"
-      } else {
-        return "Whoa, chill out!"
-      }
-    } else if (message.charCodeAt(i) !== 32 && message.charCodeAt(i) !== 9 && message.charCodeAt(i) !== 10 && message.charCodeAt(i) !== 13) {
-      empty = false
-    }
-  }
-  if(empty) {
-    return "Fine. Be that way!"
+  const blanks = /\s/g;
+  const newMessage = message.replace(blanks, "");
+  if ((/[a-z]/g.test(newMessage) || /:/g.test(newMessage))) {
+    if (newMessage[newMessage.length-1] === "?") return "Sure.";
+    else return "Whatever.";
+  } else if (/[A-Z]/g.test(newMessage)) {
+    if (newMessage[newMessage.length-1] === "?") return "Calm down, I know what I'm doing!";
+    else return "Whoa, chill out!";
+  } else if (/[0-9]/g.test(newMessage)) {
+    if (newMessage[newMessage.length-1] === "?") return "Sure.";
+    else return "Whatever.";
   } else {
-    if(message[lastIndex] === "?") {
-      return "Sure."
-    } else {
-      return "Whatever."
-    }
-  }
+    return "Fine. Be that way!"
+  };
 };
 
 /*
@@ -37,9 +22,6 @@ ignore all non letters except for last character
   yell = if every letter is in caps without ending in ? - return "Whoa, chill out!"
   yell and ask = if every letter is in caps and ending in ? - return "Calm down, I know what I'm doing!"
   empty string return "Fine. Be that way!"
-
-
-
 
   -ask a question = if one letter is lower case and ends in ? return "Sure."
   -all else return "Whatever."
